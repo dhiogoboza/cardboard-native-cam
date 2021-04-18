2e0a9a87b61b7bc3a592b5541bc71b191eaef8fb
Dhiogo Boza <dhiogoboza@gmail.com>
2021-04-18 12:26:34 -0300

    Toggle UI buttons visibility on click

--- native-cam/src/main/java/com/nvision/facetracker/MainActivity.java
+++ native-cam/src/main/java/com/nvision/facetracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nvision.facetracker;
 
+import android.animation.ObjectAnimator;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,22 +8,48 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.PopupMenu;
 
 import com.nvision.face_tracker_android.R;
 
+import java.util.ArrayList;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener, View.OnClickListener, Animation.AnimationListener {
     private static final String TAG = "MainActivity";
     private CameraRenderView mCameraView;
+    private View mCloseButton;
+    private View mSettingsButton;
+    AlphaAnimation mIncreaseOpacityAnimation;
+    AlphaAnimation mDecreaseOpacityAnimation;
+    AlphaAnimation mCurrentAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        mIncreaseOpacityAnimation = new AlphaAnimation(0.0f, 1.0f);
+        mIncreaseOpacityAnimation.setDuration(200);
+        mIncreaseOpacityAnimation.setFillAfter(true);
+
+        mDecreaseOpacityAnimation = new AlphaAnimation(1.0f, 0.0f);
+        mDecreaseOpacityAnimation.setDuration(200);
+        mDecreaseOpacityAnimation.setFillAfter(true);
+
+        mCurrentAnimation = mDecreaseOpacityAnimation;
+        mCurrentAnimation.setAnimationListener(this);
+
+        mCloseButton = findViewById(R.id.ui_close_button);
+        mCloseButton.setOnClickListener(this);
+
+        mSettingsButton = findViewById(R.id.ui_settings_button);
+        mSettingsButton.setOnClickListener(this);
 
         PermissionHelper.requestCameraPermission(this, true);
 
@@ -47,6 +74,9 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
 
         mCameraView = findViewById(R.id.camera_view);
         mCameraView.init(this);
+        mCameraView.setOnClickListener(this);
+
+        toggleButtonsVisibility();
     }
 
     private void setImmersiveSticky() {
@@ -83,12 +113,9 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         }
     }
 
-    /**
-     * Callback for when close button is pressed.
-     */
-    public void closeSample(View view) {
+    public void closeSample() {
         Log.d(TAG, "Leaving VR sample");
-        new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+        new AlertDialog.Builder(this)
                 .setTitle(R.string.closing_app)
                 .setMessage(R.string.closing_app_sure)
                 .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
@@ -101,15 +128,33 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
                 .show();
     }
 
-    /**
-     * Callback for when settings_menu button is pressed.
-     */
     public void showSettings(View view) {
         PopupMenu popup = new PopupMenu(this, view);
         MenuInflater inflater = popup.getMenuInflater();
         inflater.inflate(R.menu.main_menu, popup.getMenu());
         popup.setOnMenuItemClickListener(this);
         popup.show();
+    }
+
+    private void toggleButtonsVisibility() {
+//        Log.d(TAG, "is invisible: " + (mCloseButton.getVisibility() == View.INVISIBLE));
+//        int newVisibility = mCloseButton.getVisibility() == View.INVISIBLE ?
+//                View.VISIBLE : View.INVISIBLE;
+//
+//        mCloseButton.setVisibility(newVisibility);
+//        mSettingsButton.setVisibility(newVisibility);
+//
+//        Log.d(TAG, "new visibility: " + mCloseButton.getVisibility());
+
+        ArrayList<ObjectAnimator> animators = new ArrayList<>(); //ArrayList of ObjectAnimators
+
+        mSettingsButton.clearAnimation();
+        mCloseButton.clearAnimation();
+
+        mCurrentAnimation.setAnimationListener(this);
+
+        mCloseButton.startAnimation(mCurrentAnimation);
+        mSettingsButton.startAnimation(mCurrentAnimation);
     }
 
     @Override
@@ -120,5 +165,38 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
         }
 
         return false;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "View clicked");
+        switch (view.getId()) {
+            case R.id.ui_close_button:
+                closeSample();
+                break;
+            case R.id.ui_settings_button:
+                showSettings(view);
+                break;
+            case R.id.camera_view:
+                toggleButtonsVisibility();
+                break;
+        }
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        mCurrentAnimation = mCurrentAnimation == mDecreaseOpacityAnimation ?
+                mIncreaseOpacityAnimation : mDecreaseOpacityAnimation;
+        mCurrentAnimation.setAnimationListener(null);
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
